caccd574f0db6e040604c0b3e531ad07e091a940
JGaurav26 <gauravmakarandjadhav@gmail.com>
2025-06-23 19:32:47 +0530

    Add files via upload

---
+++ Application Programs/program168.c
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+// structure declaration
+struct node
+{
+    int data; // 4 bytes
+    struct node *next; // 8 bytes
+};
+
+typedef struct node NODE;
+typedef struct node * PNODE;
+typedef struct node ** PPNODE;
+
+void InsertFirst(struct node ** Head, int iNo)
+{
+    struct node * newn = NULL; // Create a new node
+    newn = malloc(sizeof(struct node)); // Allocate memory for the new node
+    
+    newn->data = iNo; // Assign data to the new node
+    newn->next = NULL; // Initialize the next pointer to NULL
+    
+}
+
+int main()
+{
+    struct node * First = NULL;
+    InsertFirst(&First, 10);
+    return 0;
+}
