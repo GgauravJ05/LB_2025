0a60c952a24a70c716cdd17a3d4d50c3f560997f
JGaurav26 <gauravmakarandjadhav@gmail.com>
2025-06-28 21:40:24 +0530

    Add files via upload

---
+++ Assignment/Codes/program22.c
@@ -0,0 +1,25 @@
+// PS - Write a prgram which accept one number from user and check whether that number is greater tham 100 or not
+
+#include<stdio.h>
+typedef int BOOL;
+#define TRUE 1
+#define FALSE 0
+
+BOOL ChkGreater()
+{
+
+}
+
+int main()
+{
+    int iValue = 0;
+    BOOL bRet = FALSE;
+    printf("please enter number: \n");
+    scanf("%d", iValue);
+    bRet = ();
+
+    if()
+    {
+        printf("Greater");
+    }
+}
