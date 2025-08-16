b952a5adc2b320147ce9e60f1de5753b87320c24
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-08-16 23:58:43 +0530

    Add files via upload

---
+++ Application Programs/Arithmatic2.java
@@ -0,0 +1,16 @@
+// File: fabulous/Arithmatic2.java
+package fabulous;
+
+public class Arithmatic2 {
+    public int iVal1;
+    public int iVal2;
+
+    public Arithmatic2(int A, int B) {
+        this.iVal1 = A;
+        this.iVal2 = B;
+    }
+
+    public int Addition() {
+        return (this.iVal1 + this.iVal2);
+    }
+}
