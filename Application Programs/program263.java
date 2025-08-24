cf9342f1c3c3f20e7222ca3dfa5bd38ef6d59db2
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-08-24 10:40:42 +0530

    Updating on 24 August
    current

--- Application Programs/program263.java
+++ Application Programs/program263.java
@@ -3,22 +3,22 @@ import java.util.Scanner;
 public class program263 {
     public static void main(String[] args) {
         Scanner sobj = new Scanner(System.in);
-        int iNo = 0;
+        int iNo=0;
         int iDigit = 0;
-        int iCnt = 0;
-        System.out.println("Enter the number :: ");
+        int iCount =0;
+        System.out.println("Enter Number:");
         iNo = sobj.nextInt();
 
-        while(iNo != 0)
+        while(iNo!=0)
         {
             iDigit = iNo%2;
             iNo = iNo/2;
             System.out.print(iDigit);
-            if(iDigit == 1)
+            if(iDigit==1)
             {
-                iCnt++;
+                iCount++;
             }
         }
-        System.out.println("\nNumber of 1 bit are = " +iCnt);
+        System.out.println("\n Number of 1 bit are : " + iCount);
     }
 }
