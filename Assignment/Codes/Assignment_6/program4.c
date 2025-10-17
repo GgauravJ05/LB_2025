9c2749495bc8e3b2139aa9675c3bbdea69ca1da7
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-10-17 23:52:33 +0530

    updating...
    updated all the files with their respective problem statement

--- Assignment/Codes/Assignment_6/program4.c
+++ Assignment/Codes/Assignment_6/program4.c
@@ -1,3 +1,10 @@
+/*
+    Problem Statement: Write a program which accepts number from user and print 
+    all odd numbers up to that number.
+    Input: 18
+    Output: 1 3 5 7 9 11 13 15 17
+*/
+
 #include<stdio.h>
 
 void oddDisplay(int iNo)
