b8ac1f35d85a2b2fe2f64fa0518b8405a703846e
Gaurav Jadhav <gauravmakarandjadhav@gmail.com>
2025-11-29 23:41:26 +0530

    adding the files

---
+++ Assignment/Codes/Assignment_18/program1.c
@@ -0,0 +1,40 @@
+/* 
+   Problem:
+   Accept number of rows and columns from user and display the pattern.
+
+   Example:
+   Input : iRows = 4, iCol = 3
+   Output:
+        * * *
+        * * *
+        * * *
+        * * *
+*/
+
+#include <stdio.h>
+
+void printPattern(int iRow, int iCol)
+{
+    int i = 0, j = 0;
+
+    for(i = 1; i <= iRow; i++)
+    {
+        for(j = 1; j <= iCol; j++)
+        {
+            printf("* ");
+        }
+        printf("\n");
+    }
+}
+
+int main()
+{
+    int iRow = 0, iCol = 0;
+
+    printf("Enter number of rows and columns:\n");
+    scanf("%d %d", &iRow, &iCol);
+
+    printPattern(iRow, iCol);
+
+    return 0;
+}
